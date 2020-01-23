6ee91e4c9948253f4ce4f451bae44c3c9a825cdf
Léo Gervoson <leo.gervoson@epita.fr>
2020-01-23 21:55:52 +0100

    Done Address and EncodedAddress
    Done Address based on en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses
    Done EncodedAddress

--- core/BGC/Wallet/Wallet.cs
+++ core/BGC/Wallet/Wallet.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Numerics;
+using System.Security.Cryptography;
+using BGC.Base58;
 
 namespace BGC.Wallet
 {
@@ -17,12 +19,51 @@ namespace BGC.Wallet
 
         public byte[] Address() {
             // https://en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses
-            throw new NotImplementedException();
+            
+            SHA256 sha526 = SHA256.Create();
+
+            // Step 2: SHA256 hash
+            byte[] hash = sha526.ComputeHash(PublicKey);
+
+            // Step 3: RIPEMD-160 hash
+            RIPEMD160 ripemd = RIPEMD160Managed.Create();
+            byte[] ripemdHash = ripemd.ComputeHash(hash);
+            
+
+            // Step 4: Add version byte (0x00)
+            byte[] versionned = new byte[ripemdHash.Length + 2];
+            versionned[0] = 0;
+            versionned[1] = 0;
+            for (int i = 0; i < ripemdHash.Length; i++)
+            {
+                versionned[i + 2] = ripemdHash[i];
+            }
+
+            // Step 5 & 6: SHA256 twice
+            hash = sha526.ComputeHash(versionned);
+            hash = sha526.ComputeHash(hash);
+
+            // Step 7 & 8: add trimmed hash at the end of the versionned hash
+            byte[] address = new byte[ripemdHash.Length + 6];
+            {
+                int i = 0;
+                for (; i < versionned.Length; i++)
+                {
+                    address[i] = versionned[i];
+                }
+                for (int j = 0; j < 4; j++)
+                {
+                    address[i + j] = hash[j];
+                }
+            }
+
+            return address;
         }
 
         public string EncodedAddress() {
             // Return base58 encoded version of Wallet.Address()
-            throw new NotImplementedException();
+
+            return Base58Encode.Encode(Address());
         }
     }
 
