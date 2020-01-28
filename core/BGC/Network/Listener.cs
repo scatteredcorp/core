4d952d45846a64d67cd13cd8408f4f5dce7ce7a4
Léo Gervoson <leo.gervoson@epita.fr>
2020-01-28 18:16:37 +0100

    introducing the network listener
    New file: Listener.cs
    Defines the Listener class which will server as a network Listener.
    Listens in a separate thread and enqueue received bytes.

---
+++ core/BGC/Network/Listener.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace BGC.Network
+{
+    class Listener
+    {
+        public Queue<byte[]> IncomingQueue;
+
+        private Thread listenerThread;
+
+        private bool keepListening;
+        public bool IsRunning => keepListening;
+
+        public Listener()
+        {
+            IncomingQueue = new Queue<byte[]>();
+
+            listenerThread = new Thread(new ThreadStart(listen));
+
+            keepListening = false;
+        }
+
+        public void StartListening()
+        {
+            keepListening = true;
+
+            listenerThread.Start();
+        }
+
+        public void StopListening()
+        {
+            keepListening = false;
+        }
+
+        private void listen()
+        {
+            while (keepListening)
+            {
+                // Listen
+            }
+        }
+    }
+}
