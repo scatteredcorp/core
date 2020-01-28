db4acc8cb826a7016ce9dfdfaf578c43bb99b85e
Léo Gervoson <leo.gervoson@epita.fr>
2020-01-28 18:47:22 +0100

    Added the logger class

---
+++ core/BGC/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BGC
+{
+    class Logger
+    {
+        public enum LoggingLevels
+        {
+            Debug = 3,
+            HighLogging = 2,
+            MinimalLogging = 1,
+            NoLogging = 0
+        }
+
+        public static LoggingLevels LoggingLevel = LoggingLevels.Debug;
+
+        public static void Log(string message, LoggingLevels level)
+        {
+            if (level <= LoggingLevel)
+            {
+                // Log it however you want
+                Console.Out.WriteLine(message);
+            }
+        }
+    }
+}
