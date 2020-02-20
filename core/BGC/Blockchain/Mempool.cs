75e398211ba18d8855de1d99aba45baa536f1818
unknown <aurele@oules.com>
2020-02-20 10:04:58 +0100

    mempool structure

---
+++ core/BGC/Blockchain/Mempool.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Security.Authentication.ExtendedProtection;
+using BGC.Contracts;
+
+namespace BGC.Blockchain {
+    public static class Mempool {
+        private static List<IContract> Pool { get; }
+        
+        public static void AddContract(IContract contract) {
+            Pool.Add(contract);
+        }
+
+        public static void RemoveContracts(IContract[] contracts) {
+            foreach (IContract c in contracts) {
+                Pool.Remove(c);
+            }
+        }
+    }
+}
